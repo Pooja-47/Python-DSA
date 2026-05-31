a94c59da1484b77579b1aab9825dc1936a0c9396
Pooja-47 <poojakanwar5117@gmail.com>
2026-05-31 18:58:53 +0530

    Added conditionals practice.

---
+++ python-fundamentals/Conditionals/code1.py
@@ -0,0 +1,126 @@
+# CONDITIONAL Statements::-
+""" -> These statements are used to make decisions in code.
+-> The real programs don't run the same code everytime-they make dicisions.
+    Real programs are designed to respond to different situations dynamically.
+-> Conditional statements let your program choose what to do based on a condition.
+-> They are also called control flow statements.
+
+Various conditional statements:
+1.if
+2.if-else
+3.if-elif"""
+
+# 1. if:
+"""
+*Syntax-  
+if condition :
+    statement block
+
+-> condition always returns boolean datatype either True or False.
+-> if block runs when the condition is true otherwise the interpretor skips if block.
+"""
+if True:
+    print("Hello") # the condition is true, if block runs (prints Hello)
+
+if False:
+    print("Nothing") # condition is false, so the if block is skipped by interpretor (no output)
+
+if (5 > 3 and 10 == 10) or (4 != 4 and 2 < 1) :
+    print("Hello, how are you?") # condition returns True, so if block runs (prints Hello, how are you?)
+
+if not(5 == 5 and 3 != 4) or (10 > 20):
+    print("bye") # condition returns False, if block is skipped.
+
+"""# Q1. Take age as input and tell if a person can vote or not-
+
+==>
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You can vote,:)")
+    
+
+*This only prints when someone can vote but not for those who can not vote(age less than 18).-
+-> one way for this is writing print statement outside the if block. BUT
+  this statement runs everytime even the condition is true or false because
+   it is not the part of if block.
+-> The interpretor runs code line by line so it will always print this statement 
+
+==> print("You can not vote.") # This is open statement not control flow statement
+
+-> To solve the above issue we have 
+else statement:
+
+# 2. else:
+-> No condition required.
+-> It runs only when if statement do not run.
+-> It is always written after if , it can not be written alone.
+-> Both if and else statements never run together always only one of them runs at a time.
+-> So for above problem--
+
+==> 
+else:
+    print("You can not vote")"""
+
+# 2. if-else:
+
+# Q1. Take age as input and tell if a person can vote or not-
+
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You can vote :)")
+else:
+    print("You cannot vote :(")
+
+# 3. if-elif ladder:
+""" We use this if there are more than 2 conditions.
+we can write many elif statements as per our requirement.
+For example:
+you ask your mom for money to eate something and there are three options:
+1 she gives 10 rupee note for this you buy a kurkure packt
+2 a 50 rupee note, you will buy kurkure + pepsi
+3 100 rupee note, buy a mini pizza"""
+
+rupees=int(input("Enter the amount:"))
+if rupees==10:
+    print("Eat kurkure :)")
+elif rupees==50:
+    print("Eat kurkure + pepsi ;)")
+elif rupees==100:
+    print("Eat pizza <3")
+elif rupees==500:
+    print("Woooou! parrtttyyy!!!")
+else:
+    print("Stay hungry! :'( ")
+
+# IMPORTANT NOTES:__
+"""
+==>
+if condition:
+    runs when condition is True.
+
+elif another-condition:
+    runs if the above was False, this is True.
+
+else:
+    runs when nothing above is true
+
+==>
+if   use: when you have one condition to check
+
+if-else    use: two paths-- True or False 
+
+if-elif-else   use: multiple conditions checked one by one
+
+==>
+Always, only one statement runs at a time no any two statements can run together.
+Known as ladder
+
+==>
+Priotity order:-
+if : True then exicute otherwise move to elif or else 
+
+1st elif : True then execute otherwise next elif... so on
+2nd elif
+3rd elif.... so on
+
+else : at last priority"""
---
+++ python-fundamentals/Conditionals/code2.py
@@ -0,0 +1,81 @@
+# Question practice on conditional statements:
+
+# Q1 Accept two numbers and print the greatest between them.
+num1=int(input("Enter any number:"))
+num2=int(input("Enter another number:"))
+
+"""==>
+if num1>num2:
+    print(f"{num1} is greatest.")
+else:
+    print(f"{num2} is gratest.")
+"""
+
+""" But in above program if the user gives both numbers equal as input then also it prints else statement
+To overcome this problem we use 'elif' """
+
+if num1>num2:
+    print(f"{num1} is greatest.")
+elif num2>num1:
+    print(f"{num2} is greatest.")
+else:
+    print("Both the numbers are equal.")
+
+
+# Q2 Accept gender from user and print a greeting message.
+
+gender=input("Tell your gender(M/F) please:")
+
+if gender=="F" or gender=="f":
+    print("Good morning mam")
+elif gender=="M" or gender=="m":
+    print("Good morning sir")
+else:
+    print("Others")
+
+# Q3 Accept an integer and check if it is even or odd.
+
+integer=int(input("Enter any integer:"))
+
+if integer % 2 == 0:
+    print(f"Given integer {integer} is Even.")
+else:
+    print(f"Given integer {integer} is Odd.")
+
+# Q4 Accept name and age — check if the user is a valid voter (18+).
+
+name=input("Please tell your name:")
+age=int(input("Enter your age:"))
+
+if age >= 18:
+    print(f"Congrats {name}! You can vote.") 
+else:
+    print(f"Sorry {name}! Can't vote yet. You can vote after {18 - age} years.") 
+
+# Q5 Accept a year and check if it is a leap year.
+
+year=int(input("Enter a year:"))
+
+if year % 100 == 0 and year % 400 == 0:
+    print(f"Given year is leap year")
+elif year % 100 != 0 and year % 4 == 0:
+    print("It is leap year")
+else:
+    print("Not a leap year")
+
+# Q6 — Temperature Ladder
+""" Accept temperature in °C and print a description.
+- for temp -5 to 5  print very cold
+- for 6 to 18 print cold
+-for 18 to 30 print hot""" 
+
+temp=float(input("Enter the temperature:"))
+
+if -5<=temp<=5:
+    print("Very cold")
+elif 5<temp<=18:
+    print("Cold")
+elif 18<temp<=30:
+    print("Hot")
+else:
+    print(" Temperature not in range")
