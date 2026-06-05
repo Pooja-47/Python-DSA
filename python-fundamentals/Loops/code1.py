b50df3448e902cca1a9a5e98be0dd7f09e0e0f4e
Pooja-47 <poojakanwar5117@gmail.com>
2026-06-05 23:06:46 +0530

    Added introduction to loops

---
+++ python-fundamentals/Loops/code1.py
@@ -0,0 +1,45 @@
+# Why Loops..??
+""" 
+*Imagine you have to print "Hello" 100 times.
+    without loop : 100 lines of code
+    with loop : just 2 lines
+
+Loops allow you to repeat a block of code without rewriting it .
+"""
+
+# Types of Loops:
+"""
+*Python has 2 types of loops:
+    1. for loop
+    2. while loop
+"""
+
+# Analogy to understand loops:
+"""
+*Bucket Analogy:-
+
+Let us imagine we have 2 buckets:
+    B1 bucket fully filled with water.
+    B2 empty bucket
+
+Now your task using a mug :
+    1. remove water from B1 and transfer it to B2 4 times.
+    2. remove water from B1 and transfer it to B2 until B1 is empty and B2 is full.
+
+==> In 1st task:
+    We know the exact number of times to repeat the process.(4 times)
+    So we use " for loop "
+
+==> In 2nd task:
+    We don't know the exact number but there is a condition ( till B1 empty and B2 full)
+    So we use " while loop "
+
+"""
+# Note that:
+
+"""
+* We generally use a "for" loop when we want to iterate over a sequence
+or when we know how many times a task should be repeated.
+
+* We use " while " loop when we don't know the number but there is a condition when to stop.
+"""
