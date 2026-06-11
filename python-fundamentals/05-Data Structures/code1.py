bffa617c3924cab093081e0d2a4c6ffe58f10881
Pooja-47 <poojakanwar5117@gmail.com>
2026-06-11 22:02:21 +0530

    Added introduction to data structures

---
+++ python-fundamentals/05-Data Structures/code1.py
@@ -0,0 +1,19 @@
+# Until now, we have used separate variables to store different values.
+# Creating a new value usually requires creating a new variable.
+
+""" Data Structures allow us to store multiple values in a single variable
+ in an organized and structured way."""
+
+# Examples:  Stack, Queue, Linked lists, Graph, Tree, Hashmap, List, Tuple, Set, Dictionary etc.
+
+# Data Structures:
+
+""" When you need to store multiple values in one variable, you use a data structure.
+Python gives you 4 ready to use data structures.
+
+The 4 Built-in data structures in Python:
+1 List
+2 Tuple
+3 Set
+4 Dictionary
+"""
