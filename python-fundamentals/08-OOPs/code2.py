279e4fd94de88f5b149e8d132f3274516ed6e743
Pooja-47 <poojakanwar5117@gmail.com>
2026-07-12 23:05:06 +0530

    Applied class and objects concept.

---
+++ python-fundamentals/08-OOPs/code2.py
@@ -0,0 +1,64 @@
+# Creating first class :
+"""
+Syntax:
+
+class class_name:
+    block of code
+
+Here class is keyword
+
+Inside our class we store Attributes and Methods:
+
+Attributes :
+    Variables defined inside the class are known as Attributes.
+
+Methods : 
+    Functions defined inside a class are known as Methods.
+"""
+class Car:
+    brand = "Toyota"
+
+class Animal:
+    species="Dog"  # Attribute
+
+    def make_sound():    # Method
+        print("bark")
+
+# Accessing class attributes and methods:
+"""
+A class is initialised only one time when we first run the
+program. and for accessing the attributes and methods we
+have to first access the class and then attributes and
+methods.
+"""
+# directly accessing attributes and methods using class
+
+print(Animal.species)   # accessing attribute
+Animal.make_sound()     # accessing methods
+
+# Objects :
+"""
+We can create multiple objects for same class
+
+Syntax :
+
+variable_name = class_name()
+
+here we called a class in variable outside the class
+now the variable_name is an object of class called in it
+"""
+
+class Bags:
+    name = "BagFactory"
+    def details():
+        print("This is a factory that manufactures bags")
+
+company = Bags()
+reebok = Bags()
+"""
+The object has all the powers of a class therefore a class
+object can access attributes and methods of a class.
+"""
+
+print(company.name)
+print(reebok.details)
